3c51cf3c989c46876310a3733093039138b161a0
viniciosarango <viniciosarango23@gmail.com>
2024-01-16 20:53:52 -0500

    creacion de modelos usuario y noticia

---
+++ models/noticia.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+module.exports = (sequelize, DataTypes) =>{
+    const noticia = sequelize.define('noticia', {        
+        titulo: {type:DataTypes.STRING(100), allowNull: false},
+        cuerpo: {type:DataTypes.TEXT('medio'), allowNull: false},
+        fecha: {type:DataTypes.DATE},
+        external: {type:DataTypes.UUID, defaultValue:DataTypes.UUIDv4},
+        estado: {type:DataTypes.BOOLEAN, defaultValue: true}
+    }, {freezeTableName: true});
+    return noticia;
+}
---
+++ models/usuario.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+module.exports = (sequelize, DataTypes) =>{
+    const usuario = sequelize.define('usuario', {
+        correo: {type:DataTypes.STRING(100), unique: true},
+        nombre: {type:DataTypes.STRING(100), defaultValue: "NONE"},
+        clave: {type:DataTypes.STRING(100), allowNull: false},
+        external: {type:DataTypes.UUID, defaultValue:DataTypes.UUIDv4},
+        estado: {type:DataTypes.BOOLEAN, defaultValue: true}
+    }, {freezeTableName: true});
+
+    usuario.associate = function(models) {
+        usuario.hasMany(models.noticia, { foreignKey: 'id_usuario', as: 'noticia' });
+    }    
+
+    return usuario;
+}
